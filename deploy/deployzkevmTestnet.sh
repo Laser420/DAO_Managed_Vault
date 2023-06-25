eb245de779917a9752852bbb40ee521620c78105
Chimera Defi <chimera_defi@protonmail.com>
2023-06-25 04:24:27 -0400

    add succesful zkevm testnet polygon deploy;

---
+++ deploy/deployzkevmTestnet.sh
@@ -0,0 +1,1 @@
+forge script script/DeployZkevmTestnet.s.sol:Deploy --legacy --rpc-url "https://polygon-zkevm-testnet.rpc.thirdweb.com" --sender $SENDER --private-key $WALLET_PRIVATE_KEY --broadcast -vvvv
